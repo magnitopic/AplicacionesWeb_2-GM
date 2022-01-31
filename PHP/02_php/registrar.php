3f9c914e75d0d32b1d5928eec52b85e40390168c
magnitopic <magnitopic@gmail.com>
2022-01-31 20:19:56 +0100

    Upload to DB in PHP

---
+++ PHP/02_php/registrar.php
@@ -0,0 +1,30 @@
+<?php
+//este archivo es solo PHP
+$name = $_POST["name"];
+$surname = $_POST["surname"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+
+
+//vamos a guardar la información en la base de datos db_mag
+//vamos a usar red bean php
+include "rb-mysql.php";
+//configurar red bean
+R::setup( 'mysql:host=localhost;dbname=db_mag', 'root', '' );
+
+$user = R::dispense('users');
+
+$user-> name = $name;
+$user-> surname = $surname;
+$user-> email = $email;
+$user-> phone = $phone;
+
+//con los datos listos, los guardamos en la db
+
+R::store($user);
+
+echo "Data saved succesfully. Enjoy the exibit. <br><br>";
+echo "Name: $name <br>";
+echo "Surname: $surname <br>";
+echo "Email: $email <br>";
+echo "Phone: $phone <br>";
